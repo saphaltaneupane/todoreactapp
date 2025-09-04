0f116ec21f4728fcaf22cb5beed46a52892979d2
saphaltaneupane <neupanesaphalta@gmail.com>
2025-09-04 15:56:32 +0545

    doing

---
+++ todoapp/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import styles from "./header.module.css"
+
+export default function Header() {
+    return (
+      <div className={styles.header}>
+            <h1>Todo App</h1>
+        </div>
+    );
+}
---
+++ todoapp/src/components/InlineComponent.jsx
@@ -0,0 +1,7 @@
+// const header={color: "blue", fontSize: "100px"}
+import styles from './inlinecomponent.module.css'
+export default function InlineComponent(){
+    return <div>
+        <h1 className={styles.header}>Inline Component</h1>
+    </div>
+}
